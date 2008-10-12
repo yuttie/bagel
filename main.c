c3526dcadbce02ee2094cc7630aa9c04e360cc7e
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2008-10-12 21:03:17 +0900

    Use X event for a pointer monitoring instead of busy waiting. And now exit if there are more than oen screen.

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <time.h>
+#include <stdlib.h>
 #include <X11/Xlib.h>
 
 int main() {
@@ -7,45 +7,51 @@ int main() {
     Display *display;
     if ((display = XOpenDisplay(NULL)) == NULL) {
         fprintf(stderr, "Can't connect to X server !\n");
-        return 1;
+        exit(EXIT_FAILURE);
     }
-    // ディスプレイの大きさを取得
-    int screen_num = DefaultScreen(display);
-    int width  = DisplayWidth(display, screen_num);
-    int height = DisplayHeight(display, screen_num);
+
+    // 単一スクリーンのみサポート
+    if (ScreenCount(display) != 1) {
+        fprintf(stderr, "No support for multiple screens !\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Select events
+    Window rootWindow = DefaultRootWindow(display);
+    XSelectInput(display, rootWindow, PointerMotionMask);
+
+    // イベントループ
+    XEvent event;
     for (;;) {
-        // ルートウィンドウと、そのウィンドウ上でのポインタの状態を取得
-        Window defaultRoot = DefaultRootWindow(display);
-        Window root, child;
-        int x, y, winX, winY;
-        unsigned int mask;
-        XQueryPointer(display, defaultRoot, &root, &child,
-                      &x, &y, &winX, &winY, &mask);
+        XNextEvent(display, &event);
+
         // モディファイアキーが押されていない時のみポインタを移動させる
-        if (!(mask & (ShiftMask | ControlMask | Mod1Mask))) {
+        if (!(event.xmotion.state & (ShiftMask | ControlMask | Mod1Mask))) {
+            const int screenNum = DefaultScreen(display);
+            const int width  = DisplayWidth(display, screenNum);
+            const int height = DisplayHeight(display, screenNum);
+
+            const int x = event.xmotion.x_root;
+            const int y = event.xmotion.y_root;
+            int newX = x;
+            int newY = y;
+
+            if      (x == 0)             newX = width - 2;
+            else if (x == (width - 1))   newX = 1;
+
+            if      (y == 0)             newY = height - 2;
+            else if (y == (height - 1))  newY = 1;
+
             // ポインタを移動
-            if (x == 0) {
-                XWarpPointer(display, None, root, 0, 0, width, height,
-                             width - 2, y);
-            } else if (x == (width - 1)) {
-                XWarpPointer(display, None, root, 0, 0, width, height,
-                             1, y);
-            }
-            if (y == 0) {
-                XWarpPointer(display, None, root, 0, 0, width, height,
-                             x, height - 2);
-            } else if (y == (height - 1)) {
-                XWarpPointer(display, None, root, 0, 0, width, height,
-                             x, 1);
+            if (newX != x || newY != y) {
+                XWarpPointer(display,
+                             None, rootWindow,
+                             0, 0, width, height,
+                             newX, newY);
             }
         }
-        // スリープ
-        const struct timespec req = {
-            .tv_sec  = 0,
-            .tv_nsec = 1 * 1000 * 1000
-        };
-        nanosleep(&req, NULL);
     }
+
     // Xサーバーへの接続を閉じる
     XCloseDisplay(display);
 
